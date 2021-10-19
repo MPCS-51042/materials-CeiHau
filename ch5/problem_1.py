4edcbefdaed882bf25f5fc4dc85b6b3b5cc67411
wxh <wxh@uchicago.edu>
2021-10-19 17:25:28 -0500

    ch5

--- ch5/problem_1.py
+++ ch5/problem_1.py
@@ -1,2 +1,61 @@
-class SwiftArray:
-    pass
+class SwiftArray(list):
+    def allSatisfy(self, predicate):
+        for i in self:
+            if predicate(i) == False:
+                return False
+        return True
+
+    def drop(self, predicate):
+        result = []
+        for i in self:
+            if predicate(i) == False:
+                result.append(i)
+        return result
+
+    def min(self, by=None):
+        min = self[0]
+        for i in self:
+            if by == None:
+                if i < min:
+                    min = i
+            else:
+                if by(i, min):
+                    min = i
+        return min
+
+    def partition(self, predicate):
+        tag = False
+        tail = []
+        for index, i in enumerate(self):
+            if (predicate(i) == True):
+                self.remove(i)
+                tail.append(i)
+        index = len(self)
+        self.extend(tail)
+        return index
+
+
+# dir(list)
+# dir(list.__name__)
+# print(list.__class__)
+# print(list.__mro__)
+# print(dir(list))
+# print([1, 2, 3].__contains__(0))
+#
+# print(isinstance(SwiftArray([33]), list))
+# print(isinstance(SwiftArray([33]), SwiftArray))
+# print(SwiftArray([1, 2, 3, 4]).allSatisfy(lambda x: x < 3))
+# print(SwiftArray([1, 2, 3, 4]).allSatisfy(lambda x: type(x) == int))
+# print(SwiftArray([1, 2, 3, 4]).drop(lambda x: x < 3))
+# print(SwiftArray([1, 2, -1, 4]).min())
+# print(SwiftArray(['bb', 'a', 'ccc']).min(lambda x, y: len(x) < len(y)))
+# x = SwiftArray([1, -3, 10, 5])
+# print(x.min(lambda a, b: abs(a) < abs(b)))
+# print(x.min(lambda a, b: a > b))
+# print(SwiftArray([1, 2, 3, 4]).partition(lambda x: x < 2))
+#
+# first = x.partition(lambda v: v < 0)
+# print(x)
+# print(x[first])
+
+# 30min
--- ch5/problem_4.py
+++ ch5/problem_4.py
@@ -3,5 +3,77 @@ from math import log2
 from numbers import Integral
 import re
 
+@total_ordering
 class Pitch:
-    pass
+    # class attribute
+    keys = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+
+    def __init__(self, spn_str):
+        spn_str = spn_str.upper()
+        if not spn_str[-1].isdigit():
+            raise ValueError
+        self.octave = int(spn_str[-1])
+        self.semitone = Pitch.keys.index(spn_str[0:-1])
+
+    def __add__(self, other):
+        if isinstance(other, int):
+            octave = int((self.octave * 12 + self.semitone + other) / 12)
+            semitone = (self.octave * 12 + self.semitone + other) % 12
+            spn_str = Pitch.keys[semitone] + str(octave)
+            return Pitch(spn_str)
+        else:
+            raise TypeError(NotImplemented)
+
+    def __iadd__(self, other):
+        if isinstance(other, int):
+            octave = int((self.octave * 12 + self.semitone + other) / 12)
+            semitone = (self.octave * 12 + self.semitone + other) % 12
+            self.octave = octave
+            self.semitone = semitone
+            return self
+        else:
+            raise TypeError(NotImplemented)
+
+    def __sub__(self, other):
+        if isinstance(other, int):
+            octave = int((self.octave * 12 + self.semitone - other) / 12)
+            semitone = (self.octave * 12 + self.semitone - other) % 12
+            spn_str = Pitch.keys[semitone] + str(octave)
+            return Pitch(spn_str)
+        elif isinstance(other, Pitch):
+            semitones = int((self.octave * 12 + self.semitone) - (other.octave * 12 + other.semitone))
+            return semitones
+        else:
+            raise TypeError(NotImplemented)
+
+    def __isub__(self, other):
+        if isinstance(other, int):
+            octave = int((self.octave * 12 + self.semitone - other) / 12)
+            semitone = (self.octave * 12 + self.semitone - other) % 12
+            self.octave = octave
+            self.semitone = semitone
+            return self
+        else:
+            raise TypeError(NotImplemented)
+
+    def __eq__(self, other):
+        return Pitch.frequency(self) == Pitch.frequency(other)
+
+    def __lt__(self, other):
+        return Pitch.frequency(self) < Pitch.frequency(other)
+
+    def __repr__(self):
+        spn_str = Pitch.keys[self.semitone] + str(self.octave)
+        return spn_str
+
+    def frequency(self):
+        semitones = self - Pitch('A4')
+        frequency = 440 * pow(2, (semitones) / 12)
+        return frequency
+
+    @classmethod
+    def from_frequency(cls, frequency):
+        p = Pitch('A4') + round(12 * log2(frequency / 440))
+        return p
+
+#1h
